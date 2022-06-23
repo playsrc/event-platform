a242f3c4b103765daa49392ec24a3658014c1e25
Mateus Abelli <43862225+mateusabelli@users.noreply.github.com>
2022-06-22 23:04:01 -0300

    feat: add video player and routing for lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
 import { Event } from "./pages/Event";
+import { Router } from "./Router";
 
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
