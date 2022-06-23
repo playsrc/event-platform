28e3ae33445e1b0ecea0aee3d8a7d845c1fadd14
Mateus Abelli <43862225+mateusabelli@users.noreply.github.com>
2022-06-22 23:06:26 -0300

    fix: video change issues caused by cached queries

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHCMS_API,
   cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 });
